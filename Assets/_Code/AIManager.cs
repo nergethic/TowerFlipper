3dfc54140ebd2a87fbf179ed8b9a4a3aff72d99b
Kamil Jamroz <nergydev@empyrean.games>
2019-11-17 17:59:22 +0100

    init

---
+++ Assets/_Code/AIManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIManager : MonoBehaviour {
+    [SerializeField] BattlefieldLane battlefieldLane;
+    [SerializeField] BattlefieldUnit unit;
+
+    void Start() {
+        InvokeRepeating("SpawnWarrior", 1.0f, 3.0f);
+    }
+    
+    void SpawnWarrior() {
+        Debug.LogError("Enemy unit spawned");
+        var newUnit = Instantiate(unit, battlefieldLane.enemySpawnArea.transform.position, Quaternion.identity);
+        newUnit.isEnemy = true;
+        newUnit.movementDirection = BattlefieldUnit.MovementDirection.Backward;
+    }
+}
