08e7997b1ad371272e18b650e4e5fdb47b367878
FiPiL99 <45339524+FiPiL99@users.noreply.github.com>
2019-12-05 15:26:45 +0100

    pusz

---
+++ Assets/Tury/_Code/BattlefieldLane.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattlefieldLane : MonoBehaviour {
+    public BoxCollider playerSpawnArea;
+    public BoxCollider enemySpawnArea;
+        
+    void Start()
+    {
+        
+    }
+    
+    void Update() {
+        
+    }
+}
