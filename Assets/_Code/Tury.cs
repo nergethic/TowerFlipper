e23dbbbc27f5d128cc2e8c2694876f7c4efcfae8
FiPiL99 <45339524+FiPiL99@users.noreply.github.com>
2019-12-15 22:20:34 +0100

    CameraMovement

--- Assets/_Code/Tury.cs
+++ Assets/_Code/Tury.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,9 +15,18 @@ public class Tury : MonoBehaviour {
     private GroundPlacementController placement;
     [SerializeField]
     private ResourcesManager resourcesManager;
-
+    [SerializeField] private Camera camera;
     [SerializeField] BuildingManager buildingManager;
-    
+    [SerializeField] private Transform cameraVillige;
+    [SerializeField] private Transform cameraBattlefield;
+    private Vector3 batteFieldPosition;
+    private Vector3 villigePosition;
+
+    private void Start()
+    {
+        batteFieldPosition = new Vector3(-21.13697f, 14.80627f, -2.933737f);
+    }
+
     void Update() {
         transform.eulerAngles = new Vector3(0, 0, -timeOfTurn*6);
         timeOfTurn += Time.deltaTime;
@@ -26,8 +36,28 @@ public class Tury : MonoBehaviour {
                 resourcesManager.AddResources(building.resourcesProduction);
             
             numberOfTurns++;
+            MoveCamera();
             timeOfTurn = 0;
             placement.TryToDestroySelectedObject();
         }
+        
+
     }
+
+    private void MoveCamera()
+    {
+        if (numberOfTurns % 2 == 0)
+        {
+            camera.transform.DOMove(cameraVillige.position, 0);
+            camera.transform.DORotate(cameraVillige.rotation.eulerAngles, 0);
+        }
+        else
+        {
+            camera.transform.DOMove(batteFieldPosition, 0);
+            camera.transform.DORotate(cameraBattlefield.rotation.eulerAngles, 0);
+        }
+
+    }
+
+
 }
