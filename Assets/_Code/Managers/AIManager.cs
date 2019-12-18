70b12c8a85b59648f519aac6f377ac1c2d33b16d
Kamil Jamróz <nergydev@gmail.com>
2019-12-18 23:52:53 +0100

    tower fixed

--- Assets/_Code/Managers/AIManager.cs
+++ Assets/_Code/Managers/AIManager.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class AIManager : MonoBehaviour {
     [SerializeField] UnitManager unitManager;
     [SerializeField] BattlefieldGrid battlefield;
-    [SerializeField] TowerBuilder builder;
     [SerializeField] Warrior unit;
     Grid grid = new Grid(2); // TODO make this global in HQ
 
@@ -21,8 +20,6 @@ public class AIManager : MonoBehaviour {
         var newUnit = Instantiate(unit, snappedPos, Quaternion.identity);
         newUnit.isEnemy = true;
         newUnit.movementDirection = BattlefieldUnit.MovementDirection.Backward;
-        unitManager.SpawnUnit(newUnit, snappedPos);
-        
-        builder.BuildLevel();
+        unitManager.SpawnUnit(newUnit, snappedPos); ;
     }
 }
--- Assets/_Code/TowerBuilder.cs
+++ Assets/_Code/TowerBuilder.cs
@@ -9,9 +9,11 @@ public class TowerBuilder : MonoBehaviour {
     [SerializeField] GameObject towerMiddle; // prefab object
     [SerializeField] Transform towerCenter;
     List<Transform> towerElements = new List<Transform>();
+    int index = 0;
     
     void Start() {
         towerElements.Add(towerTop.transform);
+        InvokeRepeating("ChangeTower", 4.0f, 3.0f);
     }
 
     public void BuildLevel() {
@@ -23,5 +25,36 @@ public class TowerBuilder : MonoBehaviour {
             newPos.y += towerHeight;
             towerElements[i].DOMove(newPos, 1.5f);
         }
+    }
+
+    IEnumerator DestroyFirstFloor() {
+        yield return new WaitForSeconds(1.6f);
+        int lastIndex = towerElements.Count-1;
+        var firstFloor = towerElements[lastIndex];
+        towerElements.RemoveAt(lastIndex);
+        Destroy(firstFloor.gameObject);
+    }
+    
+    public void DamageTower() {
+        for (int i = 0; i < towerElements.Count; i++) {
+            var newPos = towerElements[i].position;
+            newPos.y -= towerHeight;
+            var action = towerElements[i].DOMove(newPos, 1.5f);
+        }
+        
+        StartCoroutine(DestroyFirstFloor());
+    }
+
+    void ChangeTower() {
+        if (index % 3 == 0) {
+            BuildLevel();
+        }
+        else if (index % 3 == 1) {
+            DamageTower();
+        } else {
+            BuildLevel();
+        }
+        
+        index++;
     }
 }
